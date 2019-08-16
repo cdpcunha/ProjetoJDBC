acd80262aed16645ee6b2670c8078058f5224100
João Paulo Peixoto da Cunha <cdpcunha@hotmail.com>
2019-08-16 19:37:05 -0300

    Main 1

--- ProjetoJDBC/Programa/Main.java
+++ ProjetoJDBC/Programa/Main.java
@@ -5,6 +5,7 @@ import ObjetosEntidades.Department;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Department dep = new Department(1, "Livraria");
 		System.out.println(dep);
 
