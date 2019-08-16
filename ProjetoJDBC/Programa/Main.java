48a811ae19dd4c9c04b011526e4ade677049e4e0
João Paulo Peixoto da Cunha <cdpcunha@hotmail.com>
2019-08-16 19:15:54 -0300

    Teste

---
+++ ProjetoJDBC/Programa/Main.java
@@ -0,0 +1,13 @@
+package Programa;
+
+import ObjetosEntidades.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Department dep = new Department(1, "Livraria");
+		System.out.println(dep);
+
+	}
+
+}
