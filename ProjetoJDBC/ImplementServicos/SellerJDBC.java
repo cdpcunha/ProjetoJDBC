08ebd0e8b24a86e22d13f894c0d24d2f8a588774
João Paulo Peixoto <cdpcunha@hotmail.com>
2019-08-19 14:05:35 -0300

    Criacao dos Acessos JDBC

--- ProjetoJDBC/ImplementServicos/SellerJDBC.java
+++ ProjetoJDBC/ImplementServicos/SellerJDBC.java
@@ -1,12 +1,24 @@
 package ImplementServicos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import InterfaceServicos.SellerDAO;
+import ObjetosEntidades.Department;
 import ObjetosEntidades.Seller;
+import db.DB;
 
 public class SellerJDBC implements SellerDAO{
 
+	private Connection conn;
+	
+	public SellerJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 			
@@ -26,7 +38,34 @@ public class SellerJDBC implements SellerDAO{
 
 	@Override
 	public Seller findByID(int id) {
-		// TODO Auto-generated method stub
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps=conn.prepareStatement("SELECT S.*, D.NameDep FROM seller S JOIN department D ON S.DepartmentId = D.Id WHERE S.Id = ?");
+			ps.setInt(1, id);
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setNome(rs.getString("NameDep"));
+				
+				Seller sel = new Seller();
+				sel.setId(rs.getInt("Id"));
+				sel.setName(rs.getString("Name"));
+				sel.setEmail(rs.getString("Email"));
+				sel.setBirthDate(rs.getDate("BirthDate"));
+				sel.setSalary(rs.getDouble("BaseSalary"));
+				sel.setDepartment(dep);
+				return sel;
+			}
+		}
+		catch(SQLException e) {
+			System.out.println(e.getLocalizedMessage());
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeResultSet(rs);
+		}
 		return null;
 	}
 
--- ProjetoJDBC/Programa/Main.java
+++ ProjetoJDBC/Programa/Main.java
@@ -1,14 +1,18 @@
 package Programa;
 
-import ObjetosEntidades.Department;
+import ImplementServicos.SellerJDBC;
+import InterfaceServicos.SellerDAO;
 import ObjetosEntidades.Seller;
+import db.DB;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
-
+	
+	SellerDAO sellerDao = new SellerJDBC(DB.getConnection());	
+	Seller seller = sellerDao.findByID(6);
+	
+	System.out.println(seller);
 	}
 
 }
